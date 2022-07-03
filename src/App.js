830dad781015c1693aeb132434b0ba74d1d50ea2
astrolabs-csds <dany@astrolabs.com>
2022-07-03 12:34:57 +0400

    Theme colors and custom fonts.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 import UsersListingScreen from "./UsersListingScreen";
+
+const theme = createTheme(
+  {
+    palette: {
+      type: 'light',
+      primary: {
+        main: '#455a64',
+      },
+      secondary: {
+        main: '#f50057',
+      },
+      background: {
+        default: '#e0e0e0',
+      },
+    },
+    typography: {
+      fontFamily: 'Raleway',
+    },
+  }
+);
 
 function App() {
 
   return (
-    <UsersListingScreen/>
+    <ThemeProvider theme={theme}>
+      <UsersListingScreen/>
+    </ThemeProvider>
   )
 
 }
