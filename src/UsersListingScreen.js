7f0b4111071c65f3b95d5cc0e2e61954ec4e1cb3
astrolabs-csds <dany@astrolabs.com>
2022-07-03 12:11:29 +0400

    Work on grid and cards.

--- src/UsersListingScreen.js
+++ src/UsersListingScreen.js
@@ -1,21 +1,30 @@
 import React from 'react';
 import MUIHeader from "./MUIHeader";
+import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
+import MCard from './MCard.js';
 
 function UsersListingScreen() {
     return(
         <React.Fragment>
             <MUIHeader />
-            <Container>
-                <Grid container>
-                    <Grid></Grid>
-                    <Grid></Grid>
-                    <Grid></Grid>
-                </Grid>
-            </Container>
+            <Box mt={4}>
+                <Container>
+                    <Grid container spacing={2}>
+                        <Grid item md={4} sm={12}>
+                            <MCard imageSrc="./logo192.png"/>
+                        </Grid>
+                        <Grid item md={4} sm={12}>
+                            <MCard imageSrc="./logo192.png"/>
+                        </Grid>
+                        <Grid item md={4} sm={12}>
+                            <MCard imageSrc="./logo192.png"/>
+                        </Grid>
+                    </Grid>
+                </Container>
+            </Box>
         </React.Fragment>
-    
     )
 }
 
